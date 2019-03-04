119ac7ec22628a21c254c72005e297297ee6f90a
Cody Berlioz <codyberlioz11@gmail.com>
2019-03-03 17:58:36 -0800

    SiteV1

---
+++ js/demo.js
@@ -0,0 +1,32 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+// This can be used to set the Particles Effects. Check README for more details!
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#5cbdaa',
+    denisty: 12000,
+    proximity: 120,
+    maxSpeedX: 0.6,
+    maxSpeedY: 0.6,
+    lineColor: '#5cbdaa'
+  });
+  var intro = document.getElementById('intro');
+  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);
+
+
+/*
+// jQuery plugin example:
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+});
+*/
